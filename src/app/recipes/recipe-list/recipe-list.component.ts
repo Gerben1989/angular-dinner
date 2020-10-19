62a4fabc1c4b81293580c0b53948d331cc1bc4a6
Gerben Ostermann <gerben.ostermann@munisense.com>
2020-10-19 21:39:20 +0200

    Finished up section 3: Created a recipe and ingredient model, styled the basic components without the logic.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'Test description', 'https://i0.wp.com/www.bbq-nl.com/wp-content/uploads/2018/12/easterwoodribs-2.jpg?resize=800%2C533&ssl=1'),
+    new Recipe('Test recipe 2', 'Test description 2', 'https://i0.wp.com/www.bbq-nl.com/wp-content/uploads/2018/12/easterwoodribs-2.jpg?resize=800%2C533&ssl=1'),
+  ];
 
   constructor() { }
 
